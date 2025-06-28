253aeebc9c9706d9ee0205598af8b611564ab088
nammi narasimhamurthy <narasimhamurthynammi712@gmail.com>
2025-06-28 18:21:48 +0530

    Updated settings.py

--- exam/settings.py
+++ exam/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # For serving static files in production
 ]
 
 ROOT_URLCONF = 'exam.urls'
