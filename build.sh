bebf562f6cd76db395c99e1888ddf2b469aec372
nammi narasimhamurthy <narasimhamurthynammi712@gmail.com>
2025-06-28 18:14:52 +0530

    first commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+set -o errexit
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --no-input
+
+# Apply database migrations
+python manage.py migrate
